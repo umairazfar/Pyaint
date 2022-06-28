b4dd1f71a2d8426d2ec6ad9f57361766e1282f56
Umair Azfar Khan <umairazfar@gmail.com>
2022-06-29 00:20:01 +0500

    First Commit with Basic paint program
    First Commit. The basic paint program is working

---
+++ utils/__init__.py
@@ -0,0 +1,5 @@
+from .settings import *
+from .button import *
+import pygame
+pygame.init()
+pygame.font.init()
---
+++ utils/settings.py
@@ -0,0 +1,29 @@
+import pygame
+pygame.init()
+pygame.font.init()
+
+WHITE           = (255, 255, 255)
+BLACK           = (0, 0, 0)
+RED             = (255, 0, 0)
+GREEN           = (0, 255, 0)
+BLUE            = (0, 0, 255)
+GRAY            = (120, 120, 120)
+LIGHTGRAY       = (180, 180, 180)
+DARKGRAY        = (50, 50, 50)
+
+FPS             = 120
+
+WIDTH, HEIGHT   = 600, 700
+
+ROWS = COLS     = 40    #increase to have smaller pixels
+
+TOOLBAR_HEIGHT  = HEIGHT - WIDTH
+
+PIXEL_SIZE      = WIDTH // COLS
+
+BG_COLOR        = WHITE
+
+DRAW_GRID_LINES = True
+
+def get_font(size):
+    return pygame.font.SysFont("arial", size)
